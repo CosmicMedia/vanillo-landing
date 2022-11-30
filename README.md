b5f6b9c365f074592a7900f547be9464afd8d418
Michael Webb <therandommelonemail@gmail.com>
2022-11-29 21:40:52 -0600

    :rocket: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vanillo Landing Page
+A temporary landing page that counts down the days until Vanillo's launch.
+
+# Contributions
+Contributions will not be accepted for this repository because it will be sunset on December 25th, 2022, the day of Vanillo's launch.
---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+/*
+ *  Created by @RemiThe4ndroid for Vanillo
+ *  © Copyright 2022 CosmicMedia, LLC. 
+ */
+
+var release = (new Date("2022-12-25T00:00:00.000-05:00")).getTime() / 1000;
+var current = (new Date()).getTime() / 1000;
+
+var time;
+
+function getDays() {
+  var days = Math.floor(time / 86400);
+  time -= days * 86400;
+  return days;
+}
+
+function getHours() {
+  var hours = Math.floor(time / 3600);
+  time -= hours * 3600;
+  return hours;
+}
+
+function getMinutes() {
+  var minutes = Math.floor(time / 60);
+  time -= minutes * 60;
+  return minutes;
+}
+
+function getSeconds() {
+  return time;
+}
+
+var interval;
+
+function update() {
+  time = parseInt(release - current, 10);
+
+  if (time <= 0) clearInterval(interval);
+
+  document.getElementById('days').innerHTML    = getDays();
+  document.getElementById('hours').innerHTML   = getHours();
+  document.getElementById('minutes').innerHTML = getMinutes();
+  document.getElementById('seconds').innerHTML = getSeconds();
+
+  current++;
+}
+
+update();
+
+if (time > 0) interval = setInterval(update, 1000);
+
+// Soothing Jazz brought to you by @RemiThe4ndroid and RelaxingJazz.com
+
+var jazz = document.getElementById('jazz');
+
+jazz.volume = .2;
+
+function toggleJazz() {
+  return jazz.paused ? jazz.play() : jazz.pause();
+}
