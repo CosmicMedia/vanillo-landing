10ec8ba1c25b8c423ef6e4e31b85ed654af2f931
Hydrogen <34059898+thehydrogen@users.noreply.github.com>
2022-11-30 19:16:07 +0100

    styling updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,59 @@
 /*
- *  Created by @RemiThe4ndroid for Vanillo
- *  © Copyright 2022 CosmicMedia, LLC. 
- */
+*  Created by @RemiThe4ndroid for Vanillo
+*  © Copyright 2022 CosmicMedia, LLC. 
+*/
 
 var release = (new Date("2022-12-25T00:00:00.000-05:00")).getTime() / 1000;
 var current = (new Date()).getTime() / 1000;
 
 var time;
 
+function zero(string) {
+	string = string.toString();
+	
+	if (string.length == 1) string = '0' + string;
+
+	return string;
+}
+
 function getDays() {
-  var days = Math.floor(time / 86400);
-  time -= days * 86400;
-  return days;
+	var days = Math.floor(time / 86400);
+	time -= days * 86400;
+
+	return zero(days);
 }
 
 function getHours() {
-  var hours = Math.floor(time / 3600);
-  time -= hours * 3600;
-  return hours;
+	var hours = Math.floor(time / 3600);
+	time -= hours * 3600;
+
+	return zero(hours);
 }
 
 function getMinutes() {
-  var minutes = Math.floor(time / 60);
-  time -= minutes * 60;
-  return minutes;
+	var minutes = Math.floor(time / 60);
+	time -= minutes * 60;
+
+	return zero(minutes);
 }
 
 function getSeconds() {
-  return time;
+	return zero(time);
 }
 
 var interval;
 
 function update() {
-  time = parseInt(release - current, 10);
-
-  if (time <= 0) clearInterval(interval);
-
-  document.getElementById('days').innerHTML    = getDays();
-  document.getElementById('hours').innerHTML   = getHours();
-  document.getElementById('minutes').innerHTML = getMinutes();
-  document.getElementById('seconds').innerHTML = getSeconds();
-
-  current++;
+	time = parseInt(release - current, 10);
+	
+	if (time <= 0) clearInterval(interval);
+	
+	document.getElementById('days').innerHTML    = getDays();
+	document.getElementById('hours').innerHTML   = getHours();
+	document.getElementById('minutes').innerHTML = getMinutes();
+	document.getElementById('seconds').innerHTML = getSeconds();
+	
+	current++;
 }
 
 update();
@@ -56,5 +67,5 @@ var jazz = document.getElementById('jazz');
 jazz.volume = .2;
 
 function toggleJazz() {
-  return jazz.paused ? jazz.play() : jazz.pause();
+	return jazz.paused ? jazz.play() : jazz.pause();
 }
